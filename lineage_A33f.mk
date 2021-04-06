5434a9978e1be41cb36e8c9e6e5107c3b20f96df
Vishal Rajbhar <71400384+rvvishal@users.noreply.github.com>
2021-04-06 11:44:30 +0530

    A33f A37 ---> A33f , Update assert line for A33f , Oppo ---> OPPO

--- lineage_A33f.mk
+++ lineage_A33f.mk
@@ -16,29 +16,31 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
-$(call inherit-product, device/oppo/A37/device.mk)
+$(call inherit-product, device/oppo/A33f/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := a37f,A37f,A37fw,a37fw,msm8916,msm8939
+TARGET_OTA_ASSERT_DEVICE := a33f,A33f,A33fw,a33fw,msm8916,msm8939
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
-TARGET_VENDOR := Oppo
-PRODUCT_DEVICE := A37
-PRODUCT_NAME := lineage_A37
-BOARD_VENDOR := Oppo
-PRODUCT_BRAND := Oppo
-PRODUCT_MODEL := A37
-PRODUCT_MANUFACTURER := Oppo
+TARGET_VENDOR := OPPO
+PRODUCT_DEVICE := A33f
+PRODUCT_NAME := lineage_A33f
+BOARD_VENDOR := OPPO
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := A33f
+PRODUCT_MANUFACTURER := OPPO
 
 # Build fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="msm8916_64-user 5.1.1 LMY47V eng.root.20190711.032745 release-keys" \
-    TARGET_DEVICE="A37f"
+    TARGET_DEVICE="A33f"
 
-BUILD_FINGERPRINT := OPPO/A37fw/A37f:5.1.1/LMY47V/1519717163:user/release-keys
+BUILD_FINGERPRINT := OPPO/A33fw/A33f:5.1.1/LMY47V/1519717163:user/release-keys
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+
